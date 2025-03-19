fc09724cffac535c98a4527a7b61c3956144f6ba
Dmitriy Gaydukov <gaydukov89@gmail.com>
2025-03-19 21:27:46 +0300

    rewrite datagramchannel send to write method

--- src/main/java/com/network/raw/udp/nio/App.java
+++ src/main/java/com/network/raw/udp/nio/App.java
@@ -1,7 +1,5 @@
 package com.network.raw.udp.nio;
 
-import java.nio.channels.DatagramChannel;
-
 public class App {
     public static void main(String[] args) {
         final int serverPort = 5555;
--- src/main/java/com/network/raw/udp/nio/ChannelUdpClient.java
+++ src/main/java/com/network/raw/udp/nio/ChannelUdpClient.java
@@ -11,15 +11,15 @@ import java.nio.channels.DatagramChannel;
 @Slf4j
 public class ChannelUdpClient implements Runnable{
     private final DatagramChannel client;
-    private final InetSocketAddress serverAddress;
     private final ByteBuffer buffer = ByteBuffer.allocate(256);
 
     public ChannelUdpClient(String serverHost, int serverPort){
         try{
-            serverAddress = new InetSocketAddress(serverHost, serverPort);
+            InetSocketAddress serverAddress = new InetSocketAddress(serverHost, serverPort);
             DatagramChannel channel = DatagramChannel.open();
             // bind null, cause it's not server
             client = channel.bind(null);
+            client.connect(serverAddress);
         } catch (IOException ex){
             throw new RuntimeException(ex);
         }
@@ -37,7 +37,7 @@ public class ChannelUdpClient implements Runnable{
     private void send(String msg){
         ByteBuffer sendBuffer = ByteBuffer.wrap(msg.getBytes());
         try {
-            client.send(sendBuffer, serverAddress);
+            client.write(sendBuffer);
             SocketAddress address = client.receive(buffer);
             String received = new String(buffer.array(), 0, buffer.position());
             log.info("Client received: msg={}, address={}", received, address);
